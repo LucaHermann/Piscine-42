6bd13b37705d667b8872da046b985b438a111d1d
Hermann Luca <lhermann@student.42.fr>
2017-08-10 01:33:02 +0200

    Update ft_putnbr.c

--- push05/ex01/ft_putnbr.c
+++ push05/ex01/ft_putnbr.c
@@ -14,17 +14,7 @@ void	ft_putchar(char c);
 
 void	ft_intmin(void)
 {
-	ft_putchar('-');
-	ft_putchar('2');
-	ft_putchar('1');
-	ft_putchar('4');
-	ft_putchar('7');
-	ft_putchar('4');
-	ft_putchar('8');
-	ft_putchar('3');
-	ft_putchar('6');
-	ft_putchar('4');
-	ft_putchar('8');
+	ft_putchar("-2147483648");
 }
 
 void	ft_putnbr(int n)
